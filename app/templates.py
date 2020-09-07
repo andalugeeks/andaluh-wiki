7816ae4aea64fe7e3565c39438857458c25ef223
J. Félix Ontañón <felixonta@gmail.com>
2020-09-07 21:53:15 +0200

    Adding cookies disclaimer. EU GDPR cookies compliancy.

--- app/templates.py
+++ app/templates.py
@@ -441,6 +441,7 @@ BODY='''
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?always=1&privacyPage=https%3A%2F%2Fandaluh.es%2Fpolitica-privacidad%2F"></script>
 <script>
     var x = window.location.href;
     document.getElementById("smsharing-fb").href += x;
